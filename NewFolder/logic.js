8aa52124897202df61e6d0f85c2ad95a155267f0
Erik Stone <spologas@gmail.com>
2021-06-22 16:20:38 -0700

    trying to get the code working for set get and remove

--- NewFolder/logic.js
+++ NewFolder/logic.js
@@ -1,18 +1,71 @@
-function generatePassword() {
-    const length = 10,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (const i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+// const entries = ["John", "Smith", "Sharik", "This is stored value"];
+
+// // function myFunction(entries) {
+    
+// // }
+// $(".searchSome").on("click", function search (e) {
+//     addEntry ();
+//     var entry =JSON.parse(localStorage.getItem('allEntries'));
+//     for (let i = 0; i < entry.length; i++) {
+//         $(".showSearchData").append('<p class="keywordsOnDiv">' +entry[i] + '</p>');
+//     }
+// });
+// var existingEntries = JSON.parse(localStorage.getItem(entries));
+// function addEntry () {
+    
+//     if (existingEntries == null) existingEntries = []; 
+//     };
+//     var entryTitle = $(".searchSome").val();
+//     localStorage.setItem('entries',entryTitle);
+//     existingEntries.push(entryTitle);
+//     localStorage.setItem('allEntries', existingEntries);
+// console.log(existingEntries);
+
+// const departmentsList = [{}];
+
+// this.group.departmentsList.push({
+//     name: group.newCategoryName,
+//     sortOrder: group.departmentsList.length,
+//     type: "category"
+// });
+// function WTF () {
+//     $scope.parentDepartment = [
+//         {
+//             departmentsList: [{}]
+//         }
+//     ];
+// }
+
+// this.group.departmentsList = this.group.departmentsList || [];
+// this.group.departmentsList.push({
+//     name: group.newCategoryName,
+//     sortOrder: group.departmentsList.length,
+//     type: "category"
+// });
+
+
+const recipeName = document.querySelector("#recipeName");
+const recipeCards = document.querySelector("#recipeCards");
+const saveBtn = document.querySelector("#saveBtn");
+const deleteBtn = document.querySelector("#deleteBtn");
+const getBtn = document.querySelector("#getBtn");
+
+htmlElement.body.document.addEventListener('click', function (e){});
+ e.preventDefault();
+
+saveBtn.document.addEventListener('click', (e) =>{
+    handleRecipe();
+    reset();
+})
+
+function handleRecipe () {
+    localStorage.setItem(recipeName, recipeCards);
 }
 
-javascript:(
-    function(){
-        prompt('Here is your shiny new password:', 
-            Math.random().toString(36).slice(2) + 
-            Math.random().toString(36).slice(2)
-        );
-    }
-)();
+getBtn.document.addEventListener('click', function(){
+
+})
+
+function printItem () {
+    localStorage.getItem(recipeName).value= "userCookbook";
+}
